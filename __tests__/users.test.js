abc52be2cf7ea79e5c4f9907c0d65119d681a1fd
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-16 16:12:51 -0800

    made test to see if a user is logged in

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const fakeAccount = {
     firstname: 'Lawson',
@@ -9,6 +10,15 @@ const fakeAccount = {
     email: 'lawsonmcphetridge@gmail.com',
     password: 'supercoolpassword'
 };
+
+const registerAndLogin = async (userProps = {}) => {
+    const password = userProps.password ?? fakeAccount.password;
+    const agent = request.agent(app);
+    const user = await UserService.create({ ...fakeAccount, ...userProps });
+    const { email } = user;
+    await await agent.post('/api/v1/users/sessions').send({ email, password });
+    return [agent, user];
+}
 
 describe('secret tests', () => {
     beforeEach(() => {
@@ -27,8 +37,13 @@ describe('secret tests', () => {
 
 })
 
-
-
+    it('test to see if a user is logged in', async () => {
+        const [agent, user] = await registerAndLogin();
+        const me = await agent.get('/api/v1/users/sessions');
+        expect(me.body).toEqual({
+            ...user
+        })
+  })
 
 })
 
