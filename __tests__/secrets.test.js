6937ece6ee09b877e39e0ade79466da20a4c3b88
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-17 13:09:36 -0800

    passed secrets test

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -3,19 +3,28 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-
-
-
 describe('secret tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   it('/secrets should have a list of secrets', async () => {
-    const resp = await request(app).get('/secrets');
-    expect(resp.body).toMatchInlineSnapshot();
+    const resp = await request(app).get('/api/v1/secrets');
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "created_at": "2022-11-17T21:09:21.025Z",
+          "description": "i love coding",
+          "id": "1",
+          "title": "Super cool secret",
+        },
+        Object {
+          "created_at": "2022-11-17T21:09:21.025Z",
+          "description": "coding is cool",
+          "id": "2",
+          "title": "top secret",
+        },
+      ]
+    `);
   });
-    
-
-  
 });
