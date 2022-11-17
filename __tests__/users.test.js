ed3e674bc391232d1b91931a21d3ef4d445e28fa
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-17 12:38:43 -0800

    made test for both routes

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,50 +2,33 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+const { CookieAccessInfo } = require('cookiejar');
 
 const fakeAccount = {
-    firstname: 'Lawson',
-    lastname: 'McPhetridge',
-    email: 'lawsonmcphetridge@gmail.com',
-    password: 'supercoolpassword'
+  firstname: 'Lawson',
+  lastname: 'McPhetridge',
+  email: 'lawsonmcphetridge@gmail.com',
+  password: 'supercoolpassword'
 };
 
-const registerAndLogin = async (userProps = {}) => {
-    const password = userProps.password ?? fakeAccount.password;
-    const agent = request.agent(app);
-    const user = await UserService.create({ ...fakeAccount, ...userProps });
-    const { email } = user;
-    await await agent.post('/api/v1/users/sessions').send({ email, password });
-    return [agent, user];
-}
+
 
 describe('secret tests', () => {
-    beforeEach(() => {
-        return setup(pool);
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-    it('test to create a new user', async () => {
-        const res = await request(app).post('/api/v1/users').send(fakeAccount);
-        const { firstname, lastname, email } = fakeAccount;
-        expect(res.body).toEqual({
-            id: expect.any(String),
-            firstname,
-            lastname,
-            email
-        })
-
-})
-
-    it('test to see if a user is logged in', async () => {
-        const agent = request.agent(app);
-        const user = await UserService.create({ ...fakeAccount });
-        await agent.post('/api/v1/users/sessions').send({ email: 'test@example.com', password: '12345' });
-        const res = await agent.get('/api/v1/users/');
-        expect(res.status).toEqual(403);
-  })
-
-})
+  
+  it('POST /api/v1/sessions signs in an existing user', async () => {
+    await request(app).post('/api/v1/users').send(fakeAccount);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ firstname: 'Lawson', lastname: 'McPhetridge', email: 'lawsonmcphetridge@gmail.com', password: 'supercoolpassword' });
+    expect(res.status).toEqual(200);
+      
+      
+  });
+});
 
 
 
